35e2a9e0301f785b97e1055c79317ab26af6d5df
dmarquiroga@gmail.com <dmarquiroga@gmail.com>
2023-06-08 23:38:38 +0200

    getAllCategories gestión errores mejorados

--- src/controllers/category/getAllCategories.js
+++ src/controllers/category/getAllCategories.js
@@ -8,11 +8,20 @@ const getAllCategories = async (req, res, next) => {
 
   try {
     const [categories] = await connect.query(`SELECT * FROM category`);
-    return res.send({
-      status: 'ok',
-      message: `Has logrado obtener todas las categorías disponibles de manera exitosa`,
-      data: categories,
-    });
+    if (categories.length > 0) {
+      return res.send({
+        status: 'ok',
+        message:
+          'Has logrado obtener todas las categorías disponibles de manera exitosa',
+        data: categories,
+      });
+    } else {
+      return res.send({
+        status: 'ko',
+        error: 'No se encontraron categorías disponibles',
+        data: [],
+      });
+    }
   } catch (error) {
     next(error);
   } finally {
