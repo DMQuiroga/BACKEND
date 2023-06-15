96a39cf524bf40d0beaf0a244367a244049d9e3c
dmarquiroga@gmail.com <dmarquiroga@gmail.com>
2023-06-15 18:56:28 +0200

    añadiendo comprobación en updateUser.js, si el usuario que quiere modificar sus datos es el mismo

--- src/controllers/users/updateUser.js
+++ src/controllers/users/updateUser.js
@@ -1,82 +1,89 @@
 const getDB = require('../../database/db');
-
 const { createPathIfNotExists } = require('../../helpers/helpers');
-
 const path = require('path');
 const sharp = require('sharp');
 
 async function updateUser(req, res, next) {
-  let connect = null;
+  let connection = null;
 
   try {
     const { userId, name, surname, email, password, biography } = req.body;
 
-    connect = await getDB();
+    connection = await getDB();
 
     // Comprobar si se proporcionó id de usuario
     if (!userId) {
       return res.status(400).send({
         status: 'ko',
         error:
-          '(userId) Le recordamos un campo obligatorio proporcionarnos su id de usuario',
+          '(userId) Le recordamos que es obligatorio proporcionar su id de usuario.',
       });
     }
 
-    if (!name || name > 100) {
+    // Comprobar si el usuario actual coincide con el userId que desea modificar
+    if (req.userId !== parseInt(userId)) {
+      return res.status(403).send({
+        status: 'ko',
+        error: 'No tienes permisos para modificar estos datos de perfil.',
+      });
+    }
+
+    if (!name || name.length > 100) {
       return res.status(400).send({
         status: 'ko',
         error:
-          '(name) Le recordamos que debe proporcionarnos su nombre y debe tener una extensión máxima de 100 caracteres',
+          '(name) Le recordamos que debe proporcionarnos su nombre y debe tener una extensión máxima de 100 caracteres.',
       });
     }
-    // Comprobar la validez y longitud del texto introductorio
-    if (!surname || surname > 200) {
+
+    // Comprobar la validez y longitud de los apellidos
+    if (!surname || surname.length > 200) {
       return res.status(400).send({
         status: 'ko',
         error:
-          '(surname) Le recordamos que debe proporcionarnos sus apellidos y debe tener una extensión máxima de 200 caracteres',
+          '(surname) Le recordamos que debe proporcionarnos sus apellidos y debe tener una extensión máxima de 200 caracteres.',
       });
     }
-    // Comprobar si se proporcionó contenido textual para la noticia
+
+    // Comprobar si se proporcionó un correo electrónico
     if (!email) {
       return res.status(400).send({
         status: 'ko',
-        error: '(email) Le recordamos que debe proporcionarnos su email',
+        error:
+          '(email) Le recordamos que debe proporcionarnos su correo electrónico.',
       });
     }
 
     let photoFileName;
-    // Procesamiento de la imagen de la noticia (si se proporcionó una)
+    // Procesamiento de la imagen de perfil (si se proporcionó una)
     if (req.files && req.files.imageUrl) {
       const uploadsDir = path.join(global.__basedir, '/uploads');
       // Crear el directorio de subida si no existe
       await createPathIfNotExists(uploadsDir);
 
-      // console.log(req.files.imageUrl);
-
-      const imagenUrl = sharp(req.files.imageUrl.data);
-      imagenUrl.resize(1000);
+      const imageUrl = sharp(req.files.imageUrl.data);
+      imageUrl.resize(1000);
 
       photoFileName = `${Date.now()}_${req.files.imageUrl.name}`;
-      await imagenUrl.toFile(path.join(uploadsDir, photoFileName));
+      await imageUrl.toFile(path.join(uploadsDir, photoFileName));
     }
 
     // Verificar si el usuario existe antes de editarlo
-    const [existingUser] = await connect.query(
+    const [existingUser] = await connection.query(
       `SELECT id FROM users WHERE id = ?`,
       [userId]
     );
     if (existingUser.length === 0) {
       return res.status(404).send({
         status: 'ko',
-        error: 'El usuario solicitado no existe',
+        error: 'El usuario solicitado no existe.',
       });
     }
 
     // Actualizar los datos del usuario en la base de datos
     const lastUpdatedAt = new Date();
 
-    await connect.query(
+    await connection.query(
       `
       UPDATE users
       SET imagenUrl = ?, name = ?, surname = ?, email = ?, password = SHA2(?, 512), biography = ?, lastUpdatedAt = ?
@@ -96,7 +103,7 @@ async function updateUser(req, res, next) {
 
     return res.status(200).send({
       status: 'ok',
-      message: `Enhorabuena ${name} ${surname}, tus datos de perfil han sido actualizados exitosamente`,
+      message: `Enhorabuena ${name} ${surname}, tus datos de perfil han sido actualizados exitosamente.`,
       data: {
         updateName: name,
         updateSurname: surname,
@@ -109,19 +116,8 @@ async function updateUser(req, res, next) {
   } catch (error) {
     next(error);
   } finally {
-    if (connect) connect.release();
+    if (connection) connection.release();
   }
 }
 
 module.exports = updateUser;
-
-/*
-{
-  "name": "Davicito",
-  "surname": "Castro",
-  "email": "davidmartinezq@hotmail.com",
-  "password": "123",
-  "biography": "Soy pintor",
-  "userId": 5
-}
-*/
