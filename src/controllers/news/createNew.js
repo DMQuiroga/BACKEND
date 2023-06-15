7a01a78ec54fa96d2f9ab5a27877e674d484b33b
dmarquiroga@gmail.com <dmarquiroga@gmail.com>
2023-06-15 18:39:15 +0200

    añadiendo en createNew.js comprobación de que el usuario que quiere crear la noticia se la va a crear con su user id y no se la va a crear en la cuenta de otro usuario

--- src/controllers/news/createNew.js
+++ src/controllers/news/createNew.js
@@ -2,9 +2,7 @@
 // CREAR NUEVA NOTICIA
 
 const getConnection = require('../../database/db');
-
 const { createPathIfNotExists } = require('../../helpers/helpers');
-
 const path = require('path');
 const sharp = require('sharp');
 
@@ -14,26 +12,26 @@ const createNew = async (req, res, next) => {
   try {
     const { categoryId, title, introText, text } = req.body;
     // Comprobar la validez y longitud del título
-    if (!title || title > 250) {
+    if (!title || title.length > 250) {
       return res.status(400).send({
         status: 'ko',
         error:
-          '(tittle) Le recordamos que el título de la noticia es un campo obligatorio y debe tener una extensión máxima de 250 caracteres',
+          '(title) Le recordamos que el título de la noticia es un campo obligatorio y debe tener una extensión máxima de 250 caracteres.',
       });
     }
     // Comprobar la validez y longitud del texto introductorio
-    if (!introText || introText > 512) {
+    if (!introText || introText.length > 512) {
       return res.status(400).send({
         status: 'ko',
         error:
-          '(introText) El texto introductorio de la noticia es obligatorio y debe tener menos de 512 caracteres.',
+          '(introText) El texto introductorio de la noticia es obligatorio y debe tener una extensión máxima de 512 caracteres.',
       });
     }
     // Comprobar si se proporcionó contenido textual para la noticia
     if (!text) {
       return res.status(400).send({
         status: 'ko',
-        error: '(text) Se requiere agregar contenido textual a la noticia',
+        error: '(text) Se requiere agregar contenido textual a la noticia.',
       });
     }
     // Comprobar si se asignó una categoría a la noticia
@@ -41,7 +39,7 @@ const createNew = async (req, res, next) => {
       return res.status(400).send({
         status: 'ko',
         error:
-          '(categoryId) Es obligatorio atribuir una categoría a la noticia',
+          '(categoryId) Es obligatorio atribuir una categoría a la noticia.',
       });
     }
 
@@ -52,34 +50,42 @@ const createNew = async (req, res, next) => {
       // Crear el directorio de subida si no existe
       await createPathIfNotExists(uploadsDir);
 
-      // console.log(req.files.imageUrl);
-
-      const imagenUrl = sharp(req.files.imageUrl.data);
-      imagenUrl.resize(1000);
+      const imageUrl = sharp(req.files.imageUrl.data);
+      imageUrl.resize(1000);
 
       photoFileName = `${Date.now()}_${req.files.imageUrl.name}`;
-      await imagenUrl.toFile(path.join(uploadsDir, photoFileName));
+      await imageUrl.toFile(path.join(uploadsDir, photoFileName));
     }
 
     connection = await getConnection();
+
+    // Comprobar si el usuario actual coincide con el userId que crea la noticia
+    if (req.userId !== req.userId) {
+      return res.status(403).send({
+        status: 'ko',
+        error: 'No tienes permisos para crear esta noticia.',
+      });
+    }
+
     // Insertar una nueva noticia en la base de datos
     await connection.query(
       `
-              INSERT INTO news (
-                imagenUrl,
-                categoryId,
-                title,
-                introText,
-                text, 
-                userId)
-              VALUES (?,?,?,?,?,?)
-          `,
+      INSERT INTO news (
+        imagenUrl,
+        categoryId,
+        title,
+        introText,
+        text,
+        userId
+      )
+      VALUES (?,?,?,?,?,?)
+    `,
       [photoFileName, categoryId, title, introText, text, req.userId]
     );
 
     res.status(200).send({
       status: 'ok',
-      message: `Usted con su identificación de usuario ${req.userId}, ha creado exitosamente una nueva noticia`,
+      message: `Usted con su identificación de usuario ${req.userId}, ha creado exitosamente una nueva noticia.`,
       data: {
         userId: req.userId,
         categoryId: categoryId,
